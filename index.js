37454999c67d0b82193c01f6d89a687b6bffeff6
sravani-mandalika <sravanimadalika@gmail.com>
2026-09-09 11:43:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+
+window.alert("Good Morning!");
+
+document.getElementById("myh1").innerHTML = "Hello World";
+document.getElementById("mypara").innerHTML = "Today is a good day to learn JavaScript";
