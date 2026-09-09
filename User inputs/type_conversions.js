b4fbb5386ee933024751531a37d4d194f4b3b53a
sravani-mandalika <sravanimadalika@gmail.com>
2026-09-09 14:34:42 +0530

    added type conversions file

---
+++ User inputs/type_conversions.js
@@ -0,0 +1,11 @@
+//Type Conversions -- change the datatype of a value to another data type
+
+let x, y, z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
