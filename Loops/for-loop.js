48a385f7d2ab49ca41c1111afcea4c9437547b68
sravani-mandalika <sravanimadalika@gmail.com>
2026-09-10 14:47:11 +0530

    added while and for loops

---
+++ Loops/for-loop.js
@@ -0,0 +1,17 @@
+// for loop -- repeat some code a limited amount of times 
+
+console.log("The loop");
+for(let i = 0; i <= 2; i++) {
+    console.log(i);
+}
+
+console.log("The even numbers loop");
+for(let j = 2; j <=10; j+=2) {
+    console.log(j);
+}
+
+console.log("The odd numbers loop");
+for(let k = 3; k <=10; k+=2) {
+    console.log(k);
+}
+
